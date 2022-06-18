419836dfd84d8e2221df82951d7e2c8f16c332b9
Abdelrahman Mohamed <AdminT2@npco_net.mil>
2022-06-18 12:19:40 +0200

    Parctical Membership Control

---
+++ Python Basics/Parctical Membership Control.py
@@ -0,0 +1,32 @@
+# Admin List
+admins = ["Abdelrahman","Mohamed","Ahmed","Ali"]
+
+# Login input Name
+
+name = str(input("Enter Your Login Name: ")).strip().capitalize()
+
+# if Name in Login List
+
+if name in admins :
+    print(f"Welcome Back {name}.")
+    option = str(input("Deleted or Updated Your Name: ")).strip().capitalize()
+    if option == "Updated" or option =="U":
+        NewName = str(input("Enter a new name: ")).strip().capitalize()
+        admins[admins.index(name)]= NewName
+        print(admins)
+    elif option == "Deleted" or option =="D":
+        admins.remove(name)
+        print(admins)
+
+else:
+    print("You are't admin. ")
+    option = str(input("You want to be admin 'Yes or Not': ").strip().capitalize())
+    if option == "Yes" or option == "Y":
+        admins.append(name)
+        print("Your Name Have Been Added.")
+        print(admins)
+    elif option == "Not" or option == "N":
+        print("Thank You For using my App. Bye!!!")
+
+    else:
+        print("Wrong Choice")
