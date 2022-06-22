e861b759b8b1afe63aa561a5c652dcd34f204a56
Abdelrahman Mohamed <AdminT2@npco_net.mil>
2022-06-22 10:48:17 +0200

    multiplication_Table

--- Revision/multiplication_Table.py
+++ Revision/multiplication_Table.py
@@ -7,12 +7,26 @@ for i in range(start,end+1):
         print(f"{i} X {y} = {i*y}")
     print(15*"-")
 """
-
+"""
 def multiplication_table(start,end,ms,md):
-    for i in range(start,end):
+    for i in range(start,end+1):
         for y in range(ms,md+1):
             print(f"{i} X {y} = {i*y}")
         print(15*"-")
 
 
-multiplication_table(1,10,1,9)
+multiplication_table(1,5,1,15)
+"""
+
+class Multiplication_Table:
+    def __init__(self):
+        pass
+
+    def multiplacation(self,start,end):
+        for i in range(start,end+1):
+            for y in range(1,11):
+                print(f"{i} X {y} = {i*y}")
+            print(15*"-")
+
+m = Multiplication_Table()
+m.multiplacation(1,5)
