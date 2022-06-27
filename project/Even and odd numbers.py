8dc8fde088061726ef4b531e768b0ce6e5c4c8c9
Abdelrahman Mohamed <AdminT2@npco_net.mil>
2022-06-27 15:10:23 +0200

    project

---
+++ project/Even and odd numbers.py
@@ -0,0 +1,29 @@
+"""
+List_One = []
+List_Two = []
+count = int(input("Enter Number of numbers for the list: "))
+while count > 0 :
+    for i in range(count):
+        Public = int(input("Enter List of Number: "))
+        if Public %2 == 0:
+            List_One.append(Public)
+        elif Public %2 != 0:
+            List_Two.append(Public)
+        count -=1 
+"""
+def even_odd(List):
+    even_number = []
+    odd_number  = []
+    for number in List :
+        if number %2 == 0:
+            even_number.append(number)
+        #elif number %2 !=0:
+        else:
+            odd_number.append(number)
+    print(f"even number{even_number}")
+    print(f"odd number{odd_number}")
+
+    
+List = [1,2,3,4,5,6,7,8,9,10]  
+even_odd(List)
+
